c77f8e5f07168ace267350c0d846d940bbb5873a
Tyler <lorenarcdev@gmail.com>
2025-05-04 13:26:30 -0400

    Debugfield has been expanded to support entities, and actors.

--- include/scenes/debug_field.h
+++ include/scenes/debug_field.h
@@ -1,11 +1,17 @@
 #pragma once
+#include <vector>
+#include <memory>
 #include "base/scene.h"
+#include "base/entity.h"
 
 
 class DebugField : public Scene {
 public:
   DebugField();
+  ~DebugField();
 
   void update() override;
   void draw() override;
+private:
+  std::vector<std::unique_ptr<Entity>> entities;
 };
--- src/scenes/debug_field.cpp
+++ src/scenes/debug_field.cpp
@@ -1,16 +1,41 @@
+#include <cassert>
+#include <memory>
 #include <raylib.h>
 #include <plog/Log.h>
+#include "base/entity.h"
+#include "base/actor.h"
 #include "scenes/debug_field.h"
+
+using std::unique_ptr, std::make_unique;
 
 DebugField::DebugField() {
   PLOGI << "Initialized the DebugField Scene.";
 }
 
-void DebugField::update() {
+DebugField::~DebugField() {
+  for (unique_ptr<Entity> &entity : entities) {
+    entity.reset();
+  }
+  entities.clear();
 
+  assert(Actor::existing_actors.empty());
+  assert(Entity::existing_entities.empty());
+  PLOGI << "Unloaded the DebugField scene.";
+}
+
+void DebugField::update() {
+  for (Actor *actor : Actor::existing_actors) {
+    actor->behavior();
+  }
+
+  for (unique_ptr<Entity> &entity : entities) {
+    entity->update();
+  }
 }
 
 void DebugField::draw() {
-  DrawRectangle(0, 0, 32, 32, BLACK);
+  for (unique_ptr<Entity> &entity : entities) {
+    entity->draw();
+  }
 }
  
