f22cb616a42320c5ec3c89ae9b0dbdcadfed72dd
Tyler <lorenarcdev@gmail.com>
2025-05-08 18:53:19 -0400

    Written docstrings for the Actor class.

--- include/base/actor.h
+++ include/base/actor.h
@@ -7,10 +7,18 @@
 #include "base/entity.h"
 
 
-/* Actors are basically the main players of the Field scene.*/
+/* Actors are the main focus of the FieldScene, as do Actors in a 
+ * threatre. Typically they are used to portray characters, example 
+ * being: the game's player character, and it's npcs. Functionally, they
+ * are expected to be much more complex compared to every other entity
+ * in the scene.*/
 class Actor : public Entity {
 public:
   inline static std::set<Actor*> existing_actors;
+  /* Like entities, the game keeps track of what actors currently exist
+   * by default. For specific situations this functions helps in finding
+   * and returning an actor of a specific type. Especially useful when
+   * you know that there's only one actor that exists with that type.*/
   static Actor* getActor(enum ActorType type);
 
   Actor(std::string name, enum ActorType actor_type, Vector2 position, 
