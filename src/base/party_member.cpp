e0c033bcfd5de828b0c6828658a99f0f1af5b711
Tyler <lorenarcdev@gmail.com>
2025-06-23 11:18:56 -0400

    Defined damageMorale and increaseMorale

--- src/base/party_member.cpp
+++ src/base/party_member.cpp
@@ -1,8 +1,10 @@
 #include <raylib.h>
+#include <raymath.h>
 #include <string>
 #include "enums.h"
 #include "base/combatant.h"
 #include "base/party_member.h"
+#include <plog/Log.h>
 
 using std::string;
 int PartyMember::member_count = 0;
@@ -20,9 +22,15 @@ PartyMember::~PartyMember() {
 }
 
 void PartyMember::damageMorale(float magnitude) {
-
+  float new_morale = morale - magnitude;
+  morale = Clamp(new_morale, -max_morale, max_morale);
+  PLOGI << "Combatant: '" << name << "' [ID: " << entity_id << 
+    "] Morale has been decreased to: " << morale;
 }
 
 void PartyMember::increaseMorale(float magnitude) {
-
+  float new_morale = morale + magnitude;
+  morale = Clamp(new_morale, -max_morale, max_morale);
+  PLOGI << "Combatant: '" << name << "' [ID: " << entity_id << 
+    "] Morale has been increased to: " << morale;
 }
