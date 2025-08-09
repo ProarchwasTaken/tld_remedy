43f7cd76f27a29a3fb3833c3af06bbfbd6380e71
Tyler <lorenarcdev@gmail.com>
2025-08-09 13:54:37 -0400

    Made most of the options in the TitleScene functional.

--- include/scenes/title.h
+++ include/scenes/title.h
@@ -1,6 +1,14 @@
 #pragma once
+#include <array>
+#include <string>
+#include <raylib.h>
 #include "base/scene.h"
 
+enum class TitleOption {
+  NEW_GAME,
+  LOAD_GAME,
+  CONFIG
+};
 
 class TitleScene : public Scene {
 public:
@@ -8,5 +16,21 @@ public:
   ~TitleScene();
 
   void update() override;
+  void nextOption();
+  void prevOption();
+  void selectOption();
+
   void draw() override;
+
+  std::string getOptionText(TitleOption id);
+  void drawOptions(Font *font, int txt_size);
+private:
+  std::array<TitleOption, 3> options = {
+    TitleOption::NEW_GAME,
+    TitleOption::LOAD_GAME,
+    TitleOption::CONFIG
+  };
+
+  std::array<TitleOption, 3>::iterator selected = options.begin();
+  bool valid_session;
 };
--- src/scenes/title.cpp
+++ src/scenes/title.cpp
@@ -1,10 +1,18 @@
+#include <cassert>
 #include <raylib.h>
+#include <string>
 #include "enums.h"
 #include "game.h"
+#include "utils/input.h"
 #include "scenes/title.h"
+#include <plog/Log.h>
+
+using std::string;
+
 
 TitleScene::TitleScene() {
   scene_id = SceneID::TITLE;
+  valid_session = Game::existingSession();
 }
 
 TitleScene::~TitleScene() {
@@ -12,13 +20,98 @@ TitleScene::~TitleScene() {
 }
 
 void TitleScene::update() {
+  bool gamepad = IsGamepadAvailable(0);
+  if (Input::pressed(Game::menu_keybinds.down, gamepad)) {
+    nextOption();
+  }
+  else if (Input::pressed(Game::menu_keybinds.up, gamepad)) {
+    prevOption();
+  }
+  else if (Input::pressed(Game::menu_keybinds.confirm, gamepad)) {
+    selectOption();
+  }
+}
 
+void TitleScene::nextOption() {
+  selected++;
+
+  if (selected == options.end()) {
+    selected = options.begin();
+  }
+
+  if (!valid_session && *selected == TitleOption::LOAD_GAME) {
+    selected++;
+  }
+}
+
+void TitleScene::prevOption() {
+  if (selected == options.begin()) {
+    selected = options.end();
+  }
+
+  selected--;
+
+  if (!valid_session && *selected == TitleOption::LOAD_GAME) {
+    selected--;
+  }
+}
+
+void TitleScene::selectOption() {
+  switch (*selected) {
+    case TitleOption::NEW_GAME: {
+      Game::newSession(SubWeaponID::KNIFE, CompanionID::ERWIN);
+      break;
+    }
+    case TitleOption::LOAD_GAME: {
+      assert(valid_session);
+      Game::loadSession();
+      break;
+    }
+    case TitleOption::CONFIG: {
+      PLOGE << "This doesn't do anything yet!";
+      break;
+    }
+  }
 }
 
 void TitleScene::draw() {
   Font *font = &Game::med_font;
   int txt_size = Game::med_font.baseSize;
 
-  DrawTextEx(*font, "THT III: Remedy", {0, 0}, txt_size, -2, 
+  DrawTextEx(*font, "THT III: Remedy", {8, 8}, txt_size, -2, 
              Game::palette[14]);
+  drawOptions(font, txt_size);
+}
+
+void TitleScene::drawOptions(Font *font, int txt_size) {
+  Vector2 position = {8, 180};
+
+  for (TitleOption id : options) {
+    string text = getOptionText(id);
+
+    Color color = WHITE;
+    if (!valid_session && id == TitleOption::LOAD_GAME) {
+      color = Game::palette[2];
+    }
+    else if (*selected == id) {
+      color = Game::palette[14];
+    }
+
+    DrawTextEx(*font, text.c_str(), position, txt_size, -2, color);
+    position.y += 16;
+  }
+}
+
+string TitleScene::getOptionText(TitleOption id) {
+  switch (id) {
+    case TitleOption::NEW_GAME: {
+      return "NEW GAME";
+    }
+    case TitleOption::LOAD_GAME: {
+      return "LOAD GAME";
+    }
+    case TitleOption::CONFIG: {
+      return "CONFIG";
+    }
+  }
 }
