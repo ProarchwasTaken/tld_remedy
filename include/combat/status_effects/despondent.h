d1550d841ed8cbb204d8f12062e081f9052b4e01
Tyler <lorenarcdev@gmail.com>
2025-09-01 17:29:09 -0400

    Buffed the Despondent status ailment to be a lot more dangerous to have.

--- include/combat/status_effects/despondent.h
+++ include/combat/status_effects/despondent.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "base/party_member.h"
+#include "data/damage.h"
 #include "base/status_effect.h"
 
 
+/* Despondent is a status effect that make the afflicted very vulnerable
+ * through many different ways. Only PartyMembers can recieve this
+ * ailment, and it's when their Morale goes below zero.*/
 class Despondent : public StatusEffect {
 public:
   Despondent(PartyMember *afflicted);
@@ -10,9 +14,12 @@ public:
   void init() override;
   ~Despondent();
 
+  void intercept(DamageData &data) override;
   void logic() override;
 private:
   PartyMember *afflicted;
+
+  int intimid_lost = 0;
   
   float delay_clock = 0.0;
   float delay_time = 5;
--- src/combat/status_effects/despondent.cpp
+++ src/combat/status_effects/despondent.cpp
@@ -1,8 +1,11 @@
+#include <cmath>
 #include "enums.h"
 #include "game.h"
 #include "base/party_member.h"
 #include "base/status_effect.h"
+#include "data/damage.h"
 #include "combat/status_effects/despondent.h"
+#include <plog/Log.h>
 
 
 Despondent::Despondent(PartyMember *afflicted) : 
@@ -11,15 +14,42 @@ Despondent::Despondent(PartyMember *afflicted) :
   name = "Despondent";
   persistant = false;
   this->afflicted = afflicted;
+
+  float intimid = afflicted->intimid;
+  float dec_intimid = std::floorf(intimid * 0.90);
+  intimid_lost = intimid - dec_intimid;
+  PLOGD << "Intimidation to be lost: " << intimid_lost;
 }
 
 void Despondent::init() {
   afflicted->demoralized = true;
+
+  PLOGI << "Decreasing afflicted's intimidation by 10%";
+  afflicted->intimid -= intimid_lost;
+
+  PLOGD << "Result: " << afflicted->intimid;
 }
 
 Despondent::~Despondent() {
-  afflicted->demoralized = false;
-  afflicted->sfx.play("despondent_recover");
+  if (end) {
+    PLOGI << "Reversing stat penalties.";
+    afflicted->demoralized = false;
+    afflicted->intimid += intimid_lost;
+
+    afflicted->sfx.play("despondent_recover");
+  }
+}
+
+void Despondent::intercept(DamageData &data) {
+  if (afflicted->state != CombatantState::NEUTRAL) {
+    PLOGI << "Afflicted has taken damage while not in Neutral.";
+    data.stun_type = StunType::STAGGER;
+    data.calculation = DamageType::LIFE;
+    data.damage_type = DamageType::LIFE;
+
+    Combatant::sfx.play("damage_stagger");
+    PLOGD << "Forcing Life damage calculation and Stagger.";
+  }
 }
 
 void Despondent::logic() {
