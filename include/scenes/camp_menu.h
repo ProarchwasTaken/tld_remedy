209587225b5014658c6032281e0f883ca3b2655d
Tyler <lorenarcdev@gmail.com>
2025-09-24 13:33:05 -0400

    Had the basic stuff drawn for the Scene.

--- include/scenes/camp_menu.h
+++ include/scenes/camp_menu.h
@@ -1,7 +1,9 @@
 #pragma once
+#include <raylib.h>
 #include "base/scene.h"
 #include "data/session.h"
 #include "data/keybinds.h"
+#include "system/sprite_atlas.h"
 
 
 class CampMenuScene : public Scene {
@@ -10,8 +12,26 @@ public:
   ~CampMenuScene();
 
   void update() override;
+  void offsetBars();
+
   void draw() override;
+  void drawTopBar();
+  void drawHeader(Vector2 position);
+  void drawTopInfo(Vector2 position);
+
+  void drawBottomBar();
+  void drawBottomInfo(Vector2 position);
+
+  static SpriteAtlas atlas;
 private:
   Session *session;
   MenuKeybinds *keybinds;
+
+  Texture main_bar;
+
+  float bar_offset = 0;
+  float offset_speed = 110;
+
+  const Vector2 top_position = {0, 0};
+  const Vector2 bottom_position = {0, 200};
 };
--- src/scenes/camp_menu.cpp
+++ src/scenes/camp_menu.cpp
@@ -1,11 +1,15 @@
 #include <cassert>
 #include <raylib.h>
+#include <raymath.h>
 #include "enums.h"
 #include "game.h"
 #include "data/session.h"
 #include "utils/input.h"
+#include "system/sprite_atlas.h"
 #include "scenes/camp_menu.h"
 #include <plog/Log.h>
+
+SpriteAtlas CampMenuScene::atlas("menu", "camp_menu");
 
 
 CampMenuScene::CampMenuScene(Session *session) {
@@ -13,9 +17,15 @@ CampMenuScene::CampMenuScene(Session *session) {
   this->scene_id = SceneID::CAMP_MENU;
   this->session = session;
   this->keybinds = &Game::settings.menu_keybinds;
+
+  atlas.use();
+  main_bar = atlas.getTexturefromSprite(3);
 }
 
 CampMenuScene::~CampMenuScene() {
+  atlas.release();
+  UnloadTexture(main_bar);
+  assert(atlas.users() == 0);
   PLOGI << "Unloading the Camp Menu Scene.";
 }
 
@@ -24,8 +34,63 @@ void CampMenuScene::update() {
   if (Input::pressed(keybinds->cancel, gamepad)) {
     Game::returnToField();
   }
+
+  offsetBars();
+}
+
+void CampMenuScene::offsetBars() {
+  if (bar_offset > 4) {
+    bar_offset = bar_offset - 4;
+  }
+
+  bar_offset += offset_speed * Game::deltaTime();
 }
 
 void CampMenuScene::draw() {
+  drawTopBar();
+  drawBottomBar();
+}
 
+void CampMenuScene::drawTopBar() {
+  Rectangle source = {0 + bar_offset, 0, 426, 40};
+  Vector2 position = top_position;
+  DrawTextureRec(main_bar, source, position, WHITE);
+
+  drawHeader(position);
+  drawTopInfo(position);
+}
+
+void CampMenuScene::drawHeader(Vector2 position) {
+  Rectangle *sprite = &atlas.sprites[4];
+  position = Vector2Add(position, {9, 16});
+  DrawTextureRec(atlas.sheet, *sprite, position, WHITE);
+}
+
+void CampMenuScene::drawTopInfo(Vector2 position) {
+  Rectangle *sprite = &atlas.sprites[6];
+  position = Vector2Add(position, {176, 22});
+  DrawTextureRec(atlas.sheet, *sprite, position, WHITE);
+
+  Font *font = &Game::med_font;
+  int txt_size = font->baseSize;
+  position = Vector2Add(position, {11, 1});
+
+  DrawTextEx(*font, "The Outside - Cathedral", position, txt_size, -2, 
+             WHITE);
+}
+
+void CampMenuScene::drawBottomBar() {
+  Rectangle source = {0 + bar_offset, 0, -426, -40};
+  Vector2 position = bottom_position;
+  Rectangle dest = {position.x, position.y, 426, 40};
+  DrawTexturePro(main_bar, source, dest, {0, 0}, 0, WHITE);
+
+  drawBottomInfo(position);
+}
+
+
+void CampMenuScene::drawBottomInfo(Vector2 position) {
+  Rectangle *sprite = &atlas.sprites[5];
+  position = Vector2Add(position, {32, 4});
+  DrawTextureRec(atlas.sheet, *sprite, position, WHITE);
 }
