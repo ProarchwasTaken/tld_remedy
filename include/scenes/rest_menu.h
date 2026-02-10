9df60fa5426aee1cfb4b4307124401174c56f6c1
Tyler <lorenarcdev@gmail.com>
2026-02-10 18:56:32 -0500

    Got the menu options drawn.

--- include/scenes/rest_menu.h
+++ include/scenes/rest_menu.h
@@ -1,10 +1,24 @@
 #pragma once
+#include <array>
+#include <cstddef>
+#include <unordered_set>
 #include <string>
 #include <raylib.h>
 #include "base/scene.h"
 #include "data/session.h"
+#include "data/keybinds.h"
+#include "system/sound_atlas.h"
 #include "system/sprite_atlas.h"
 #include "combat/hud/blackbars.h"
+
+
+enum class RestMenuOptions {
+  DIAGNOSE,
+  CRAFTING,
+  TALK,
+  SAVE,
+  LEAVE
+};
 
 
 class RestMenuScene : public Scene {
@@ -17,10 +31,19 @@ public:
 
   void update() override;
   void openingLogic();
+
+  void optionNavigation();
   void flickeringLogic();
+
   void draw() override;
+  void drawBackground();
+  void drawOptions();
+  void selectedLogic(float clock, Vector2 &position, Color &color);
+  const char *getOptionText(RestMenuOptions option);
 private:
   Session *session;
+  MenuKeybinds *keybinds;
+
   Camera2D camera;
   BlackBars black_bars = BlackBars(120, true);
 
@@ -39,11 +62,27 @@ private:
     READY
   } state = OPENING;
 
+  std::array<RestMenuOptions, 5> options = {
+    RestMenuOptions::DIAGNOSE,
+    RestMenuOptions::CRAFTING,
+    RestMenuOptions::TALK,
+    RestMenuOptions::SAVE,
+    RestMenuOptions::LEAVE
+  };
+
+  std::array<RestMenuOptions, 5>::iterator selected = options.begin();
+  std::array<RestMenuOptions, 5>::iterator prev_selected = NULL;
+  std::unordered_set<RestMenuOptions> disallowed;
+
   float state_clock = 0.0;
   float state_time = 2.0;
 
   float tick_clock = 0.0;
   float tick_time = 1.0 / 60.0;
 
+  float opt_clock = 0.0;
+  float opt_time = 0.2;
+
   SpriteAtlas atlas = SpriteAtlas("menu", "rest_menu");
+  SoundAtlas *sfx;
 };
--- src/scenes/rest_menu.cpp
+++ src/scenes/rest_menu.cpp
@@ -9,6 +9,8 @@
 #include "game.h"
 #include "data/session.h"
 #include "utils/math.h"
+#include "utils/menu.h"
+#include "utils/input.h"
 #include "scenes/rest_menu.h"
 
 using std::string, std::uniform_int_distribution;
@@ -17,7 +19,9 @@ using std::string, std::uniform_int_distribution;
 RestMenuScene::RestMenuScene(Session *session) {
   assert(session != NULL);
   scene_id = SceneID::REST_MENU;
+
   this->session = session;
+  keybinds = &Game::settings.menu_keybinds;
 
   loadBackground(session->location);
   camera.target = {0, 0};
@@ -31,11 +35,16 @@ RestMenuScene::RestMenuScene(Session *session) {
 
   atlas.use();
   setupCharacters();
+
+  sfx = &Game::menu_sfx;
+  sfx->use();
 }
 
 RestMenuScene::~RestMenuScene() {
   UnloadTexture(background);
   atlas.release();
+
+  sfx->release();
 }
 
 void RestMenuScene::loadBackground(string location) {
@@ -70,11 +79,12 @@ void RestMenuScene::update() {
       break;
     }
     case READY: {
-      // Remove this later!
-      if (IsKeyPressed(KEY_K)) {
-        state = OPENING;
+      if (opt_clock < 1.0) {
+        opt_clock += Game::deltaTime() / opt_time;
+        opt_clock = Clamp(opt_clock, 0.0, 1.0);
       }
 
+      optionNavigation();
       flickeringLogic();
       break;
     }
@@ -109,6 +119,28 @@ void RestMenuScene::openingLogic() {
   }
 }
 
+void RestMenuScene::optionNavigation() {
+  if (opt_clock < 1.0) {
+    return;
+  }
+
+  bool gamepad = IsGamepadAvailable(0);
+  if (Input::down(keybinds->down, gamepad)) {
+    prev_selected = selected;
+    MenuUtils::nextOption(options, selected, &disallowed);
+
+    opt_clock = 0.0;
+    sfx->play("menu_navigate");
+  }
+  else if (Input::down(keybinds->up, gamepad)) {
+    prev_selected = selected;
+    MenuUtils::prevOption(options, selected, &disallowed);
+
+    opt_clock = 0.0;
+    sfx->play("menu_navigate");
+  }
+}
+
 void RestMenuScene::flickeringLogic() {
   tick_clock += Game::deltaTime() / tick_time;
   if (tick_clock < 1.0) {
@@ -139,12 +171,17 @@ void RestMenuScene::flickeringLogic() {
   }
 
   number = range(Game::RNG);
-  if (number < 2) {
-    bg_color.a = 128;
+  if (number == 50) {
+    bg_color.a = 200;
   }
 }
 
 void RestMenuScene::draw() {
+  drawBackground();
+  drawOptions();
+}
+
+void RestMenuScene::drawBackground() {
   BeginMode2D(camera);
   { 
     DrawTextureV(background, {-250, -150}, bg_color);
@@ -153,4 +190,85 @@ void RestMenuScene::draw() {
     DrawTextureRec(atlas.sheet, *com_sprite, com_position, char_color);
   }
   EndMode2D();
+}
+
+void RestMenuScene::drawOptions() {
+  Font *font = &Game::med_font;
+  int txt_size = font->baseSize;
+
+  int offset = (options.size() - 1) - disallowed.size();
+  Vector2 base_position = {-16.0f, 226.0f - (offset* 16)};
+
+  for (RestMenuOptions &option : options) {
+    if (disallowed.find(option) != disallowed.end()) {
+      continue;
+    }
+
+    Vector2 position = base_position;
+    Color color = Game::palette[2];
+
+    if (&option == selected) {
+      selectedLogic(opt_clock, position, color);
+    }
+    else if (&option == prev_selected) {
+      selectedLogic(1.0 - opt_clock, position, color);
+    }
+
+    if (state == OPENING) {
+      float percentage = Clamp((-0.70 + state_clock) / 0.30, 0.0, 1.0);
+      position.x = Math::smoothstep(position.x - 32, position.x, 
+                                    percentage);
+      color.a = 255 * percentage; 
+    }
+
+    DrawTextureRec(atlas.sheet, atlas.sprites[0], position, color);
+
+    const char *text = getOptionText(option);
+    position = Vector2Add(position, {23, -10});
+    DrawTextEx(*font, text, position, txt_size, -2, color);
+
+    base_position.y += 16;
+  }
+}
+
+void RestMenuScene::selectedLogic(float clock, Vector2 &position, 
+                                  Color &color)
+{
+  if (clock == 1.0) {
+    color = WHITE;
+    position.x += 16;
+    return;
+  }
+  else if (clock == 0.0) {
+    return;
+  }
+
+  position.x += Math::smoothstep(0, 16, clock);
+
+  Color start_color = Game::palette[2];
+  unsigned char r = Lerp(start_color.r, 255, clock);
+  unsigned char g = Lerp(start_color.g, 255, clock);
+  unsigned char b = Lerp(start_color.b, 255, clock);
+
+  color = {r, g, b, 255};
+}
+
+const char *RestMenuScene::getOptionText(RestMenuOptions option) {
+  switch (option) {
+    case RestMenuOptions::DIAGNOSE: {
+      return "DIAGNOSE";
+    }
+    case RestMenuOptions::CRAFTING: {
+      return "CRAFTING";
+    }
+    case RestMenuOptions::TALK: {
+      return "TALK";
+    }
+    case RestMenuOptions::SAVE: {
+      return "SAVE";
+    }
+    case RestMenuOptions::LEAVE: {
+      return "LEAVE";
+    }
+  }
 }
