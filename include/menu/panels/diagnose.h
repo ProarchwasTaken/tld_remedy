0e0634d07651a5706eacd917095b8bf2aa4ef61d
Tyler <lorenarcdev@gmail.com>
2026-02-16 19:56:09 -0500

    Implemented character portraits to the panel along with basic menu navigation.

--- include/menu/panels/diagnose.h
+++ include/menu/panels/diagnose.h
@@ -1,9 +1,11 @@
+#include <array>
 #include <raylib.h>
 #include "base/panel.h"
 #include "data/keybinds.h"
 #include "data/session.h"
 #include "system/sprite_atlas.h"
 #include "system/sound_atlas.h"
+#include "menu/hud/portrait.h"
 
 
 class DiagnosePanel : public Panel {
@@ -13,13 +15,16 @@ public:
 
   void update() override;
   void heightLerp();
+  void menuNavigation();
 
   void draw() override;
+  void drawMemberName();
+  void drawPortrait();
 private:
   SpriteAtlas *menu_atlas;
   SpriteAtlas *rest_atlas;
 
-  MenuKeybinds *keybinds;
+  MenuKeybinds *keybind;
   SoundAtlas *sfx;
 
   float percentage = 0.0;
@@ -27,4 +32,8 @@ private:
   Texture frame;
   float frame_height = 0;
   constexpr static Vector2 frame_position = {76, 24};
+
+  std::array<Character*, 2> party;
+  std::array<Character*, 2>::iterator current_member;
+  Portrait portrait = Portrait({81, 54});
 };
--- src/menu/panels/diagnose.cpp
+++ src/menu/panels/diagnose.cpp
@@ -1,18 +1,26 @@
 #include <cassert>
+#include <cctype>
 #include <cstddef>
+#include <string>
 #include <raylib.h>
 #include "base/panel.h"
 #include "enums.h"
 #include "game.h"
 #include "data/session.h"
+#include "utils/text.h"
+#include "utils/input.h"
+#include "utils/menu.h"
 #include "system/sprite_atlas.h"
 #include "menu/panels/diagnose.h"
 #include <plog/Log.h>
+
+using std::string;
 
 
 DiagnosePanel::DiagnosePanel(Session *session, SpriteAtlas *rest_atlas) {
   id = PanelID::DIAGNOSE;
   frame = LoadTexture("graphics/menu/frames/diagnose.png");
+  keybind = &Game::settings.menu_keybinds;
 
   menu_atlas = &Game::menu_atlas;
   menu_atlas->use();
@@ -22,6 +30,9 @@ DiagnosePanel::DiagnosePanel(Session *session, SpriteAtlas *rest_atlas) {
 
   sfx = &Game::menu_sfx;
   sfx->use();
+
+  party = {&session->player, &session->companion};
+  current_member = party.begin();
   PLOGD << "DiagnosePanel has been initialized.";
 }
 
@@ -35,6 +46,11 @@ void DiagnosePanel::update() {
     transitionLogic();
     heightLerp();
   }
+  else {
+    menuNavigation();
+  }
+
+  portrait.update(percentage);
 }
 
 void DiagnosePanel::heightLerp() {
@@ -48,7 +64,65 @@ void DiagnosePanel::heightLerp() {
   frame_height = 192 * percentage;
 }
 
+void DiagnosePanel::menuNavigation() {
+  if (portrait.fade_clock != 1.0) {
+    return;
+  }
+
+  bool gamepad = IsGamepadAvailable(0);
+  if (Input::pressed(keybind->right, gamepad)) {
+    MenuUtils::nextOption(party, current_member);
+    sfx->play("menu_navigate");
+    portrait.fade_clock = 0.0;
+  }
+  else if (Input::pressed(keybind->left, gamepad)) {
+    MenuUtils::prevOption(party, current_member);
+    sfx->play("menu_navigate");
+    portrait.fade_clock = 0.0;
+  }
+  else if (Input::pressed(keybind->cancel, gamepad)) {
+    state = PanelState::CLOSING;
+    sfx->play("menu_cancel");
+  }
+}
+
 void DiagnosePanel::draw() {
   Rectangle source = {0, 0, 274, frame_height};
   DrawTextureRec(frame, source, frame_position, WHITE);
+
+  if (state == PanelState::READY) {
+    drawMemberName();
+    drawPortrait();
+  }
+}
+
+void DiagnosePanel::drawMemberName() {
+  Font *font = &Game::med_font;
+  int txt_size = font->baseSize;
+
+  Character *party_member = *current_member;
+  std::string name = party_member->name;
+
+  Vector2 position = TextUtils::alignCenter(name.c_str(), {250, 25}, 
+                                            *font, -2, 0);
+  DrawTextEx(*font, name.c_str(), position, txt_size, -2, WHITE);
+}
+
+void DiagnosePanel::drawPortrait() {
+  Character *party_member = *current_member;
+  PartyMemberID id = party_member->member_id;
+
+  Font *font = &Game::med_font;
+  int txt_size = font->baseSize;
+
+  std::string name = party_member->name;
+  for (char &letter : name) {
+    letter = std::toupper(letter);
+  }
+
+  Vector2 position = TextUtils::alignCenter(name.c_str(), {112, 198}, 
+                                            *font, -2, 0);
+
+  DrawTextEx(*font, name.c_str(), position, txt_size, -2, WHITE);
+  portrait.draw(id);
 }
